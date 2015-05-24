762e03f8168570b275164f556221b0ed2e855191
katerniles <katerussell@sbcglobal.net>
2015-05-24 16:28:34 -0400

    lesson 16
    had issues with naming and order of jquery; solved

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 /*! scripts.js */
-$(document).ready(function() {
+/*$(document).ready(function() {
 	alert("Document is ready!");
 }); /*testing to make sure connected correctly. It is*/
 
-$("#comment").css("background-color", "orange"); 
-
+$(".comment").css("background-color", "pink"); 
+	
 $("#btn-info").on("click", function () {
+	//when submit button is clicked, 
+	var comment = $(".comment").val();
+	// Grab the value of the textbox and set it to a variable
 	console.log("clicked");	
-	var comment = $("#comment").val();
+	//adds "clicked" to console log
+	console.log(comment);
+	//makes sure right data is retrieved
+	$("#visible-comment").html(comment);
+	//displays textbox content
 	return false;
+	//doesn't submit input since no where to go
 });
-
+	
