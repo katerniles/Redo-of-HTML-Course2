1e1541ed1e434c6ce4f13696679968a99be9c1b8
katerniles <katerussell@sbcglobal.net>
2015-05-26 20:45:11 -0400

    lesson 17 for-loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,48 @@
 /*! scripts.js */
-/*$(document).ready(function() {
+/*testing to make sure connected correctly. It is*/
+$(document).ready(function() {
 	alert("Document is ready!");
-}); /*testing to make sure connected correctly. It is*/
+	/*create zebra-striped table */
+	var rows = $(".my-row");
+	for(var i=0; i<rows.length; ++i) {
+		if (i%2===0) {
+			$(rows[i]).css("background-color", "yellow");
+	};
+};
+}); 
+$(".comment").css("background-color", "white"); 
 
-$(".comment").css("background-color", "pink"); 
-	
-$("#btn-info").on("click", function () {
 	//when submit button is clicked, 
-	var comment = $(".comment").val();
+$("#btn-info").on("click", function () {
 	// Grab the value of the textbox and set it to a variable
-	console.log("clicked");	
+	var comment = $(".comment").val();
 	//add "clicked" to console log
+	console.log("clicked");	
+	//make sure right data is retrieved
 	console.log(comment);
-	//makes sure right data is retrieved
+	//display textbox content
 	$("#visible-comment").html(comment);
-	//displays textbox content
+	//don't submit input since no where to go
 	return false;
-	//doesn't submit input since no where to go
 });
 $(".comment").on("keyup", function() {
-	console.log("keyup happened"); 
 		//make sure we're caching keyup
+	console.log("keyup happened"); 
 	var nameName = 3;
 	var name = "string";
+	//set length of the content of the textarea to a variable
 	var charCount = $(".comment").val().length;
-	//sets length of the content of the textarea to a variable
-	console.log(charCount);
 	//make sure set to right value	
-	$("#char-count").html(charCount);
+	console.log(charCount);
 	//show running character count to the user
+	$("#char-count").html(charCount);
+		//need to turn character count red
 	if(charCount > 50) {
 		$("#char-count").css("color", "red");
-		//need to turn character count red
+		//need to be black
 	} else {
 		$("#char-count").css("color", "black");
-		//need to be black
 	};
 	//here is where rest of code goes
 });
 
-$(".twitter-follow-button").on("click", function() {
-	//when twitter button is clicked, 
-	console.log("One more follower!");
-	//add "message" to console log
-});
